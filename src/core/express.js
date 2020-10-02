7f6b6d64dc9e0c933ba78743b25c75622b27573c
Javier Garrido <javierrock7@gmail.com>
2020-10-01 23:14:11 -0300

    add cors

--- src/core/express.js
+++ src/core/express.js
@@ -1,11 +1,13 @@
 import 'babel-core/register';
 import 'babel-polyfill';
+import cors from 'cors';
 import Express from 'express';
 import accessLogger from 'middlewares/accessLogger';
 import responseHeaders from 'middlewares/responseHeaders';
 
 const app = new Express();
 
+app.use(cors())
 app.use(responseHeaders);
 app.use(accessLogger);
 export default app;
